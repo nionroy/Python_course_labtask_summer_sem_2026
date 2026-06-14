6989781ef4369f57a3d76481650a8d571def6071
nion-roy <nionroy18@gmail.com>
2026-06-14 12:05:08 +0600

    Initial commit with Lab task 1 2 and 3

---
+++ Lab_1/python_script.py
@@ -0,0 +1,14 @@
+_myvar = 23
+print(_myvar)
+
+username =input("Enter your name: ")
+print("Hello " , username)
+print("Hello " + username)
+print(type(username))
+
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(num1 + num2)
+
---
+++ Lab_2/swap.py
@@ -0,0 +1,12 @@
+_first_var = 10
+_second_var = 20
+print("Before swapping: ")
+print("First variable: ", _first_var)
+print("Second variable: ", _second_var)
+# Swapping the values
+_temp_var = _first_var  
+_first_var = _second_var
+_second_var = _temp_var
+print("After swapping: ")
+print("First variable: ", _first_var)
+print("Second variable: ", _second_var) 
